64ab8357150fbafb2b021073b5644184937339df
shenke <shenke@winupon.com>
2022-11-06 16:42:44 +0800

    完善Autumn

--- autumn-context/src/main/java/com/autumn/context/Context.java
+++ autumn-context/src/main/java/com/autumn/context/Context.java
@@ -1,13 +1,14 @@
 package com.autumn.context;
 
 import com.autumn.core.Coordinate;
+import com.autumn.core.CoordinateHolder;
 import org.jetbrains.annotations.NotNull;
 
 /**
  * @author momo
  * @since 2022/9/19 22:08
  */
-public interface Context<R extends StandardRequest> {
+public interface Context<R extends StandardRequest> extends CoordinateHolder {
 
 
     /**
@@ -23,6 +24,7 @@ public interface Context<R extends StandardRequest> {
      *
      * @return coordinate
      */
+    @Override
     @NotNull
     Coordinate getCoordinate();
 }
---
+++ autumn-core/src/main/java/com/autumn/core/CoordinateHolder.java
@@ -0,0 +1,10 @@
+package com.autumn.core;
+
+/**
+ * @author shenke
+ * @since 2022/11/6 16:34
+ */
+public interface CoordinateHolder {
+
+    Coordinate getCoordinate();
+}
